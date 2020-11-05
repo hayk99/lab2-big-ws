3f30c3eaf97c8523ab217c921cd1c10d36948946
hayk99 <haykk1999@gmail.com>
2020-11-05 11:58:27 +0100

    primary goal done

--- src/main/java/translator/infrastructure/DummyTranslator.java
+++ src/main/java/translator/infrastructure/DummyTranslator.java
@@ -7,6 +7,7 @@ public class DummyTranslator extends TranslatorImpl {
 
   @Override
   protected String translateInternal(String from, String to, String text, String encodedText) {
-    return "I don't know how to translate from "+from+" to "+ to + " the text '"+text+"'";
+    String msg = "I don't know how to translate from "+from+" to "+ to + " the text '"+text+"'";
+    throw new RuntimeException(msg);
   }
 }
--- src/test/java/translator/web/ws/TranslatorEndpointTest.java
+++ src/test/java/translator/web/ws/TranslatorEndpointTest.java
@@ -36,13 +36,41 @@ public class TranslatorEndpointTest {
     marshaller.afterPropertiesSet();
   }
 
-  @Test
+  @Test(expected = RuntimeException.class)
   public void testSendAndReceive() {
     GetTranslationRequest request = new GetTranslationRequest();
     request.setLangFrom("en");
     request.setLangTo("es");
     request.setText("This is a test of translation service");
     Object response = new WebServiceTemplate(marshaller).marshalSendAndReceive("http://localhost:"
+            + port + "/ws", request);
+    assertNotNull(response);
+    assertThat(response, instanceOf(GetTranslationResponse.class));
+    GetTranslationResponse translation = (GetTranslationResponse) response;
+    assertThat(translation.getTranslation(), is("I don't know how to translate from en to es the text 'This is a test of translation service'"));
+  }
+
+  @Test(expected = RuntimeException.class)
+  public void testNetworkFailure() {
+    GetTranslationRequest request = new GetTranslationRequest();
+    request.setLangFrom("en");
+    request.setLangTo("es");
+    request.setText("This is a test of translation service");
+    Object response = new WebServiceTemplate(marshaller).marshalSendAndReceive("http://localhost:"
+            + port + "/wsEstoNoExiste", request);
+    assertNotNull(response);
+    assertThat(response, instanceOf(GetTranslationResponse.class));
+    GetTranslationResponse translation = (GetTranslationResponse) response;
+    assertThat(translation.getTranslation(), is("I don't know how to translate from en to es the text 'This is a test of translation service'"));
+  }
+
+  @Test(expected = RuntimeException.class)
+  public void testRemoteServiceFailure() {
+    GetTranslationRequest request = new GetTranslationRequest();
+    request.setLangFrom("en");
+    request.setLangTo("es");
+    request.setText("This is a test of translation service");
+    Object response = new WebServiceTemplate(marshaller).marshalSendAndReceive("http://localhostTT:"
             + port + "/ws", request);
     assertNotNull(response);
     assertThat(response, instanceOf(GetTranslationResponse.class));
